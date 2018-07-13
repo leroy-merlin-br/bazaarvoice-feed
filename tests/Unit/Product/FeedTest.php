db574e5f8453b37a35cf6743db74835d53beb0e4
Diego Felix <diegoflx.oliveira@gmail.com>
2018-07-13 10:53:46 -0300

    standartize tests files

--- tests/Unit/Product/FeedTest.php
+++ tests/Unit/Product/FeedTest.php
@@ -1,78 +1,119 @@
 <?php
 namespace BazaarVoice\Product;
 
+use BazaarVoice\Elements\BrandElementInterface;
+use BazaarVoice\Elements\CategoryElement;
+use BazaarVoice\Elements\FeedElementInterface;
+use BazaarVoice\Elements\ProductElement;
+use Exception;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStreamWrapper;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 
-class FeedTest extends TestCase {
+class FeedTest extends TestCase
+{
+    /** @test */
+    public function it_creates_a_new_feed_element()
+    {
+        // Set
+        $feed = new Feed();
+        $name = 'itsANewFeedName';
 
-  public function testNewFeedElement() {
-    $pf = new Feed();
-    $name = substr( md5(rand()), 0, 8);
-    $feed = $pf->newFeed($name);
-    $this->assertInstanceOf('BazaarVoice\Elements\FeedElementInterface', $feed);
-  }
+        // Actions
+        $feed = $feed->newFeed($name);
 
-  public function testNewBrandElement() {
-    $pf = new Feed();
-    $name = substr( md5(rand()), 0, 8);
-    $id = 'test_brand';
-    $brand = $pf->newBrand($id, $name);
-    $this->assertInstanceOf('BazaarVoice\Elements\BrandElementInterface', $brand);
-  }
-
-  public function testNewCategoryElement() {
-    $pf = new Feed();
-    $name = substr( md5(rand()), 0, 8);
-    $id = 'test_category';
-    $page_url = 'http://www.example.com/' . $id;
-    $category = $pf->newCategory($id, $name, $page_url);
-    $this->assertInstanceOf('BazaarVoice\Elements\CategoryElement', $category);
-  }
-
-  public function testNewProductElement() {
-    $pf = new Feed();
-    $name = substr( md5(rand()), 0, 8);
-    $id = 'test_product';
-    $page_url = 'http://www.example.com/' . $id;
-    $image_url = $page_url . '/' . $name . '.jpg';
-    $product = $pf->newProduct($id, $name, 'test_category', $page_url, $image_url);
-    $this->assertInstanceOf('BazaarVoice\Elements\ProductElement', $product);
-  }
-
-  /**
-   * Test generating valid XML document.
-   */
-  public function testPrintFeed() {
-    $pf = new Feed();
-    $feed = $pf->newFeed('testFeed');
-    $xml_string = $pf->printFeed($feed);
-    new \SimpleXMLElement($xml_string);
-
-    $prev = libxml_use_internal_errors(true);
-    try {
-      new \SimpleXMLElement($xml_string);
-    } catch(\Exception $e) {
+        // Assertions
+        $this->assertInstanceOf(FeedElementInterface::class, $feed);
     }
-    $this->assertCount(0, libxml_get_errors());
-    // Tidy up.
-    libxml_clear_errors();
-    libxml_use_internal_errors($prev);
-  }
 
-  /**
-   * Test saving Feed gzipped XML file.
-   */
-  public function testSaveFeed() {
-    $test_feed = 'testFeed_' . substr( md5(rand()), 0, 10);
-    $pf = new Feed();
-    $feed = $pf->newFeed($test_feed);
-    vfsStreamWrapper::register();
-    vfsStreamWrapper::setRoot(new vfsStreamDirectory($test_feed . '_dir'));
-    $pf->saveFeed($feed, vfsStream::url($test_feed . '_dir'), $test_feed);
-    $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild($test_feed . '.xml.gz'));
-  }
+    /** @test */
+    public function it_creates_a_new_brand_element()
+    {
+        // Set
+        $feed = new Feed();
+        $name = 'itsANewFeedName';
+        $brandId = 'some-brand';
 
+        // Actions
+        $brand = $feed->newBrand($brandId, $name);
+
+        // Assertions
+        $this->assertInstanceOf(BrandElementInterface::class, $brand);
+    }
+
+    /** @test */
+    public function it_creates_a_new_category_element()
+    {
+        // Set
+        $feed = new Feed();
+        $name = 'itsANewFeedName';
+        $categoryId = 'categoryId';
+        $pageUrl = 'http://www.example.com/' . $categoryId;
+
+        // Actions
+        $category = $feed->newCategory($categoryId, $name, $pageUrl);
+
+        // Assertions
+        $this->assertInstanceOf(CategoryElement::class, $category);
+    }
+
+    /** @test */
+    public function it_creates_a_new_product_element()
+    {
+        // Set
+        $feed = new Feed();
+        $name = 'itsANewFeedName';
+        $productId = 'someProductId123';
+        $productUrl = 'http://www.example.com/' . $productId;
+        $imageUrl = "{$productUrl}/{$name}.jpg";
+
+        // Actions
+        $product = $feed->newProduct($productId, $name, 'someCategoryId', $productUrl, $imageUrl);
+
+        // Assertions
+        $this->assertInstanceOf(ProductElement::class, $product);
+    }
+
+    /** @test */
+    public function it_prints_a_feed()
+    {
+        // Set
+        $feed = new Feed();
+        $feedElement = $feed->newFeed('testFeed');
+        $xmlString = $feed->printFeed($feedElement);
+
+        // Actions
+        new SimpleXMLElement($xmlString);
+        $prev = libxml_use_internal_errors(true);
+
+        try {
+            new SimpleXMLElement($xmlString);
+        } catch(Exception $e) {}
+
+        // Assertions
+        $this->assertCount(0, libxml_get_errors());
+
+        // TearDown
+        libxml_clear_errors();
+        libxml_use_internal_errors($prev);
+    }
+
+    /** @test */
+    public function it_saves_a_feed()
+    {
+        // Set
+        $testFeedFile = 'testFeed';
+        $feed = new Feed();
+
+        // Actions
+        $feedElement = $feed->newFeed($testFeedFile);
+        vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory($testFeedFile . '_dir'));
+        $feed->saveFeed($feedElement, vfsStream::url($testFeedFile . '_dir'), $testFeedFile);
+
+        // Assertions
+        $this->assertTrue(vfsStreamWrapper::getRoot()->hasChild($testFeedFile . '.xml.gz'));
+    }
 }
